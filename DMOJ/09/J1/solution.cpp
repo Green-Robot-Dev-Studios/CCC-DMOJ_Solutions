295472773f020f5f03aaa63f16f3b6d4f746fc31
Khalid Filali <56809036+KhalidFilali@users.noreply.github.com>
2020-01-24 14:31:35 -0500

    Create solution.cpp

---
+++ DMOJ/09/J1/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int x;
+int c;
+int f;
+
+cin>>x;
+cin>>c;
+cin>>f;
+int g=((9*1)+(7*3)+(8*1)+(0*3)+(9*1)+(2*3)+(1*1)+(4*3)+(1*1)+(8*3)+(x*1)+(c*3)+(f*1));
+cout<<"The 1-3-sum is "<<g<<endl;
+
+	return 0;
+}
