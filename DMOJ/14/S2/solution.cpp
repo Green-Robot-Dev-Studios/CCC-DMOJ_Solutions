8f9f477b8092e23908971163fe0e5f4cfa4b953b
Green-Robot-Dev-Studios <nficara01@gmail.com>
2020-01-24 12:16:10 -0500

    Update solution.cpp

--- DMOJ/14/S2/solution.cpp
+++ DMOJ/14/S2/solution.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 int main() {
-  int maax, cars, carsList[100006];
-  cin >> maax >> cars;
-  for (int i = 0; i < cars; i++){
-    cin >> carsList[i];
+  string in1[30];
+  string in2[30];
+  int words;
+  cin >> words;
+  for (int i = 0; i < words; i++){
+    cin >> in1[i];
   }
-  for (int i = 4; i <= cars+4; i++){
-    int totes = 0;
-    for (int ii = i-4; ii < i; ii++){
-      if (ii < 0){
-        continue;
-      }
-      totes += carsList[ii];
-      if (totes > maax){
-        cout << ii;
-        return 0;
+  for (int i = 0; i < words; i++){
+    cin >> in2[i];
+  }
+  for (int i = 0; i < words; i++){
+    if (in1[i] == in2[i]){
+      cout << "bad";
+      return 0;
+    } 
+    for (int ii = 0; ii < i; ii++){
+      if (in2[ii] == in1[i]){
+        //cout << in2[ii] << " is equal to " << in1[i] << endl;
+        if (in1[ii] != in2[i]){
+          //cout << in1[ii] << " is equal to " << in2[i] << endl;
+          cout << "bad";
+          return 0;
+        }
       }
     }
   }
+  cout << "good";
 }
