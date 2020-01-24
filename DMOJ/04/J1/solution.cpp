e97122e12d2a196d9be59809fe2e70f68fdf5a3e
Khalid Filali <56809036+KhalidFilali@users.noreply.github.com>
2020-01-24 14:36:12 -0500

    Create solution.cpp

---
+++ DMOJ/04/J1/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main(){
+
+float x;
+cin>>x;
+int a=sqrt(x);
+cout<<"The largest square has side length "<<a<<"."<<endl;
+   return 0;
+}
