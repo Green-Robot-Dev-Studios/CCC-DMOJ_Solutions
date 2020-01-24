9204549419877ba38cda599a5e00a9f20afd5398
Green-Robot-Dev-Studios <nficara01@gmail.com>
2020-01-24 14:03:55 -0500

    Create solution.cpp

---
+++ DMOJ/10/J1/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+using namespace std;
+
+int input;
+
+int main(){
+
+	cin >> input;
+
+	switch (input){
+		case 1 : cout << "1";
+				 return 0;
+		case 2 : cout << "2";
+				 return 0;
+		case 3 : cout << "2";
+				 return 0;
+		case 4 : cout << "3";
+				 return 0;
+		case 5 : cout << "3";
+				 return 0;
+		case 6 : cout << "3";
+				 return 0;
+		case 7 : cout << "2";
+				 return 0;
+		case 8 : cout << "2";
+				 return 0;
+		case 9 : cout << "1";
+				 return 0;
+	}
+
+	int _;
+	cin >> _;
+}
